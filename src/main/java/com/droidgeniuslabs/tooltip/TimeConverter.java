21bca5a52b13bb42aee6696508e45fa15887a4c5
NotYezz <notyestv@gmail.com>
2025-04-10 09:43:38 -0400

    Visual edit

--- src/main/java/com/droidgeniuslabs/tooltip/TimeConverter.java
+++ src/main/java/com/droidgeniuslabs/tooltip/TimeConverter.java
@@ -1,4 +1,39 @@
 package com.droidgeniuslabs.tooltip;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class TimeConverter {
+   private static final List<String> units = Arrays.asList("Picoseconds (ps)", "Microsecond (μs)", "Millisecond (ms)", "Second (s)", "Minute (m)","Hour (h)", "Day (d)", "Week (wk)", "Year (y)");
+    public ComboBox inputUnitComboBox;
+    public ComboBox outputUnitComboBox;
+    public TextField inputField;
+    public TextField outputField;
+
+    @FXML
+    public void initialize(){
+        inputUnitComboBox.getItems().addAll(units);
+        outputUnitComboBox.getItems().addAll(units);
+        inputUnitComboBox.setValue("Picoseconds (ps)");
+        outputUnitComboBox.setValue("Microseconds (μs)");
+    }
+
+    public void handleTimeConvert() {
+        try{
+            double inputValue = Double.parseDouble(inputField.getText().trim());
+            String inputUnit = inputUnitComboBox.getValue().toString();
+            String outputUnit = outputUnitComboBox.getValue().toString();
+        }catch (NumberFormatException e){
+            outputField.setText("Invalid input.");
+        }
+    }
+
+    private void timeConverter(double inValue, String inputUnit, double outValue, String outputUnit){
+
+    }
 }
